d9d5e6dbbc50be8556b46cf6f59ace1c6f1c2596
Khanh Nguyen <dinhkhanh412@gmail.com>
2021-01-05 05:06:09 +0700

    Admin: Can edit, cannot add and delete

--- Source/project_manager/lib/Screens/Admin/admin.dart
+++ Source/project_manager/lib/Screens/Admin/admin.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:project_manager/Screens/Admin/viewProfile.dart';
 import 'package:project_manager/Screens/Home/home_page.dart';
 import 'package:project_manager/Screens/Profile/profile.dart';
+import 'package:project_manager/Screens/Project/project_page.dart';
 import 'package:project_manager/constants.dart';
 import 'package:project_manager/components/google_nav_bar.dart';
 import 'package:project_manager/Screens/Calendar/calendar_page.dart';
-import 'package:project_manager/Screens/Project/project_page.dart';
+//import 'package:project_manager/Screens/Project/project_page.dart';
 import 'package:http/http.dart' as http;
 
 class AdminPage extends StatefulWidget {
@@ -19,76 +21,15 @@ class AdminPage extends StatefulWidget {
 
 class _AdminPageState extends State<AdminPage> {
   int selectedIndex = 4;
-  final TextStyle dropdownMenuItem =
-      TextStyle(color: Colors.black, fontSize: 18);
+  final TextStyle dropdownMenuItem = TextStyle(color: Colors.black, fontSize: 18);
 
   final primary = Color(0xff696b9e);
   final secondary = Color(0xfff29a94);
-  // final List<Map> schoolLists = [
-  //   {
-  //     "name": "Edgewick Scchol",
-  //     "location": "572 Statan NY, 12483",
-  //     "type": "Higher Secondary School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_960_720.png"
-  //   },
-  //   {
-  //     "name": "Xaviers International",
-  //     "location": "234 Road Kathmandu, Nepal",
-  //     "type": "Higher Secondary School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2017/01/31/13/14/animal-2023924_960_720.png"
-  //   },
-  //   {
-  //     "name": "Kinder Garden",
-  //     "location": "572 Statan NY, 12483",
-  //     "type": "Play Group School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2016/06/09/18/36/logo-1446293_960_720.png"
-  //   },
-  //   {
-  //     "name": "WilingTon Cambridge",
-  //     "location": "Kasai Pantan NY, 12483",
-  //     "type": "Lower Secondary School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2017/01/13/01/22/rocket-1976107_960_720.png"
-  //   },
-  //   {
-  //     "name": "Fredik Panlon",
-  //     "location": "572 Statan NY, 12483",
-  //     "type": "Higher Secondary School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_960_720.png"
-  //   },
-  //   {
-  //     "name": "Whitehouse International",
-  //     "location": "234 Road Kathmandu, Nepal",
-  //     "type": "Higher Secondary School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2017/01/31/13/14/animal-2023924_960_720.png"
-  //   },
-  //   {
-  //     "name": "Haward Play",
-  //     "location": "572 Statan NY, 12483",
-  //     "type": "Play Group School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2016/06/09/18/36/logo-1446293_960_720.png"
-  //   },
-  //   {
-  //     "name": "Campare Handeson",
-  //     "location": "Kasai Pantan NY, 12483",
-  //     "type": "Lower Secondary School",
-  //     "logoText":
-  //         "https://cdn.pixabay.com/photo/2017/01/13/01/22/rocket-1976107_960_720.png"
-  //   },
-  // ];
-
   List employees;
 
   getMethod() async {
     String url = "https://phuidatabase.000webhostapp.com/getEmployee.php";
-    var res = await http
-        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
     var body = json.decode(res.body);
     return body;
   }
@@ -97,11 +38,15 @@ class _AdminPageState extends State<AdminPage> {
     return Text(
       title,
       style: TextStyle(
-          color: kDarkBlue,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w700,
-          letterSpacing: 1.2),
+          color: kDarkBlue, fontSize: 20.0, fontWeight: FontWeight.w700, letterSpacing: 1.2),
     );
+  }
+
+  ImageProvider getProfilePicture(dynamic info) {
+    if (info['ProfilePhotoLink'] == "" || info['ProfilePhotoLink'] == null)
+      return AssetImage('assets/images/avatar.png');
+    else
+      return NetworkImage(info['ProfilePhotoLink']);
   }
 
   @override
@@ -114,40 +59,15 @@ class _AdminPageState extends State<AdminPage> {
               child: SingleChildScrollView(
                 child: Column(
                   children: <Widget>[
+                    Container(),
                     Container(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 20.0),
-                      child: Material(
-                        elevation: 5.0,
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                        child: TextField(
-                          // controller: TextEditingController(text: locations[0]),
-                          cursorColor: Theme.of(context).primaryColor,
-                          style: dropdownMenuItem,
-                          decoration: InputDecoration(
-                              hintText: "Search Employee",
-                              hintStyle: TextStyle(
-                                  color: Colors.black38, fontSize: 16),
-                              prefixIcon: Material(
-                                elevation: 0.0,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30)),
-                                child: Icon(Icons.search),
-                              ),
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 25, vertical: 13)),
-                        ),
-                      ),
-                    ),
-                    Container(
+                      color: Colors.blueGrey[300],
                       padding: EdgeInsets.only(top: 0),
                       height: MediaQuery.of(context).size.height,
                       width: double.infinity,
                       child: FutureBuilder(
                         future: getMethod(),
-                        builder:
-                            (BuildContext context, AsyncSnapshot snapshot) {
+                        builder: (BuildContext context, AsyncSnapshot snapshot) {
                           if (employees != null) {
                             return ListView.builder(
                               itemCount: employees.length,
@@ -157,8 +77,7 @@ class _AdminPageState extends State<AdminPage> {
                             );
                           }
                           employees = snapshot.data;
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
                             return Center(
                               child: CircularProgressIndicator(),
                             );
@@ -185,8 +104,7 @@ class _AdminPageState extends State<AdminPage> {
                     offset: Offset(0, 15))
               ]),
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
                 child: GNav(
                     gap: 8,
                     color: Colors.grey[800],
@@ -218,27 +136,22 @@ class _AdminPageState extends State<AdminPage> {
                       if (index == 0) {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(
-                              builder: (context) => HomePage(id: widget.id)),
+                          MaterialPageRoute(builder: (context) => HomePage(id: widget.id)),
                         );
                       } else if (index == 2) {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  CalendarPage(id: widget.id)),
+                          MaterialPageRoute(builder: (context) => CalendarPage(id: widget.id)),
+                        );
+                      } else if (index == 1) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ProjectPage(id: widget.id)),
                         );
                       } else if (index == 3) {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(
-                              builder: (context) => ProfilePage(id: widget.id)),
-                        );
-                      } else if (index == 4) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => AdminPage(id: widget.id)),
+                          MaterialPageRoute(builder: (context) => ProfilePage(id: widget.id)),
                         );
                       }
                     }),
@@ -251,85 +164,64 @@ class _AdminPageState extends State<AdminPage> {
   }
 
   Widget buildList(BuildContext context, int index, List employees) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: Colors.white,
-      ),
-      width: double.infinity,
-      height: 110,
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            width: 50,
-            height: 50,
-            margin: EdgeInsets.only(right: 15),
-            // decoration: BoxDecoration(
-            //   borderRadius: BorderRadius.circular(50),
-            //   border: Border.all(width: 3, color: secondary),
-            //   image: DecorationImage(
-            //       //image: NetworkImage(schoolLists[index]['logoText']),
-            //       fit: BoxFit.fill),
-            // ),
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ViewProfilePage(id: widget.id, info: employees[index])),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.white,
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "userName:  " + employees[index]['userName'],
-                  style: TextStyle(
-                      color: primary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
+          width: double.infinity,
+          height: 110,
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 80,
+                height: 80,
+                margin: EdgeInsets.only(right: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(width: 3, color: Colors.blueGrey),
+                  image:
+                      DecorationImage(image: getProfilePicture(employees[index]), fit: BoxFit.fill),
                 ),
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Icon(
-                      Icons.location_on,
-                      color: secondary,
-                      size: 20,
+                    Text(
+                      "Name:  " +
+                          employees[index]['FirstName'] +
+                          ' ' +
+                          employees[index]['LastName'],
+                      style: TextStyle(color: primary, fontWeight: FontWeight.bold, fontSize: 20),
                     ),
                     SizedBox(
-                      width: 5,
+                      height: 6,
                     ),
-                    Text("role:  " + employees[index]['role'],
-                        style: TextStyle(
-                            color: primary, fontSize: 13, letterSpacing: .3)),
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.location_on,
-                      color: secondary,
-                      size: 20,
-                    ),
+                    Text("Role:  " + employees[index]['role'],
+                        style: TextStyle(color: primary, fontSize: 17)),
                     SizedBox(
-                      width: 5,
+                      height: 5,
                     ),
                     Text("Salary:  " + employees[index]['Salary'],
-                        style: TextStyle(
-                            color: primary, fontSize: 13, letterSpacing: .3)),
+                        style: TextStyle(color: primary, fontSize: 17)),
                   ],
                 ),
-                SizedBox(
-                  height: 6,
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+              ),
+              Divider(),
+            ],
+          ),
+        ));
   }
 }
